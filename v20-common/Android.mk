f2a24a179a02199383dce76adf9c5f192510b37f
Matt Filetto <matt.filetto@gmail.com>
2017-03-09 16:17:34 -0800

    v20-common: Add HotwordEnrollment apk for now until opengapps gets their S#@T together
    Change-Id: I010433bb8c7b269b9554eeb241d0883eb312b168
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- v20-common/Android.mk
+++ v20-common/Android.mk
@@ -19,4 +19,16 @@ LOCAL_PATH := $(call my-dir)
 
 ifneq ($(filter v20 h910 h918 us996 ls997 vs995,$(TARGET_DEVICE)),)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE := HotwordEnrollment
+LOCAL_MODULE_OWNER := lge
+LOCAL_SRC_FILES := proprietary/priv-app/HotwordEnrollment/HotwordEnrollment.apk
+LOCAL_CERTIFICATE := platform
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := APPS
+LOCAL_DEX_PREOPT := false
+LOCAL_MODULE_SUFFIX := .apk
+LOCAL_PROPRIETARY_MODULE := true
+include $(BUILD_PREBUILT)
+
 endif
