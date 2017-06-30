ba6f82bd4ef44ed22130dabcaaa30e4fffa9c25a
Matt Filetto <matt.filetto@gmail.com>
2017-06-29 17:40:25 -0700

    update ims apks from op3
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- msm8996-common/Android.mk
+++ msm8996-common/Android.mk
@@ -139,17 +139,6 @@ LOCAL_MODULE_SUFFIX := .apk
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := SimSettings
-LOCAL_MODULE_OWNER := lge
-LOCAL_SRC_FILES := proprietary/app/SimSettings/SimSettings.apk
-LOCAL_CERTIFICATE := platform
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := APPS
-LOCAL_DEX_PREOPT := false
-LOCAL_MODULE_SUFFIX := .apk
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE := TimeService
 LOCAL_MODULE_OWNER := lge
 LOCAL_SRC_FILES := proprietary/app/TimeService/TimeService.apk
